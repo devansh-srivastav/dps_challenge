38d6b4cda9b07c10df72d74dd4b9d98f86c180ee
Devansh Srivastav <devansh.srivastav@gmail.com>
2021-10-12 22:46:05 +0530

    Deploying FastAPI

---
+++ app.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+import json
+from prediction import get_prediction
+import pandas as pd
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get('/')
+async def home():
+    body = '''
+        <h1>DPS Task API Endpoint Devansh Srivastav</h1>
+        <p>Check the docs: <a href='/docs'> here</a></p>
+    '''
+    return HTMLResponse(content=body, status_code=200)
+
+
+@app.get('/prediction')
+async def get_result(year: str, month: str):
+    return json.loads(get_prediction(year, month).to_json(orient='records'))[0]
--- prediction.py
+++ prediction.py
@@ -7,5 +7,6 @@ model = load_model('model_for_dps')
 def get_prediction(year,month):
     df = pd.DataFrame([[int(year), int(month)]], columns=['JAHR', 'MONAT'])
     prediction = predict_model(model, data=df)
-    return int(prediction.values[0][2])
+    prediction = prediction.astype({"Label": int})
+    return pd.DataFrame([[prediction.iloc[:, 2].values[0]]], columns=['prediction'])
 
